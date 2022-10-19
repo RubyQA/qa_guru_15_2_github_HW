8d93677acd89a093c756ab6e59e6f52a659e8b68
Dmitrii.Korobkin <Dmitri.Korobkin@nordigy.ru>
2022-10-19 23:45:29 +0400

    new test

--- src/test/java/exampleTest.java
+++ src/test/java/exampleTest.java
@@ -1,14 +1,16 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 public class exampleTest {
     @Test
     void exampleTest(){
-        Assertions.assertTrue(2>1);
+        Assertions.assertTrue(new Random().nextInt()>1);
     }
 
     @Test
     void exampleTest1(){
-        Assertions.assertTrue(1>2);
+        Assertions.assertTrue(3 == 3);
     }
 }
